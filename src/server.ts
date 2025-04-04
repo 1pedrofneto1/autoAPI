576a152cf6e2ed394702e42a9dabfa9e3b3d0723
Pedro Fernandes <pednetocn@gmail.com>
2025-04-04 14:59:25 -0300

    feat/add and config swagger

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,15 @@
 import express from "express";
+import swaggerUi from "swagger-ui-express";
+import YAML from "yamljs";
 
 import { router } from "./routes";
+
+const swaggerFile = YAML.load("./src/swagger.yaml");
 
 const app = express();
 
 app.use(express.json());
-
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use(router);
 
 app.listen(3333, () => console.log("Server is running!"));
